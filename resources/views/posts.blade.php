be0a4aa0fcb0d693139deea085d9d93664352903
vianepw <viane.213040103@mail.unpas.ac.id>
2023-11-22 23:27:29 +0700

    Video 8. Post Category & Eloquent Relationship

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,19 @@
+@extends('layouts.main')
+
+@section('container')
+
 @foreach ($posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-5">
     <h2>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
     </h2>
-    <p>{{ $post->body }}</p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+    <p>{{ $post->excerpt }}</p>
+
+
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More</a>
 </article>
 @endforeach
+
+@endsection
