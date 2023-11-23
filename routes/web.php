a5b9e58fa73b2f828c3dd152e50827affb00989c
vianepw <viane.213040103@mail.unpas.ac.id>
2023-11-23 13:57:01 +0700

    Video 14. View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-
-use App\Models\Category;
 use App\Models\User;
+use App\Models\Category;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -48,3 +50,6 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
